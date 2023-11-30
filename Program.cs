a398f58fe0d129b985f2a88b4d4b27b7f1309953
Mirzezadesemed1995 <samedmirzezade@gmail.com>
2023-11-30 21:26:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      
+
+        Print("text");
+
+    }
+ 
+
+ 
+
+
+    public static void Print(string text)
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine(text);
+    }
+}
